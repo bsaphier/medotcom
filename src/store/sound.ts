4883ca264e55086668bf82023d6cffe873810682
Benjamin Saphier <b.saphier@gmail.com>
2024-03-07 01:21:53 -0500

    trigger buildd

--- src/store/sound.ts
+++ src/store/sound.ts
@@ -1,11 +1,15 @@
-import { create } from "zustand";
+import { Scale, bluesScale } from 'sound';
+import { create } from 'zustand';
 
 interface SoundState {
     droneOn: boolean;
     toggleDrone: () => void;
+    scale: Scale;
 }
 
 export const useSoundStore = create<SoundState>((set) => ({
     droneOn: false,
     toggleDrone: () => set((state) => ({ droneOn: !state.droneOn })),
+    scale: bluesScale,
+    setScale: (nextScale: Scale) => set({ scale: nextScale }),
 }));
